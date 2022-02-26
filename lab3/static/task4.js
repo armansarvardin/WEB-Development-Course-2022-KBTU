8ee2f4bd5bd3744b514833e189e0f8d10efa3536
Arman Sarvadin <78083072+armansarv07@users.noreply.github.com>
2022-02-26 15:29:25 +0600

    Zabyl zapushit'

---
+++ lab3/static/task4.js
@@ -0,0 +1,2 @@
+const BIRTHDATE = '18.04.1982';
+var age = someCode(birthday);
---
+++ lab3/task_logical.js
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <script>
+            alert( 5 > 4 );
+            alert( "apple" > "pineapple" );
+            alert( "2" > "12" );
+            alert( undefined == null );
+        </script>
+    </body>
+</html>
