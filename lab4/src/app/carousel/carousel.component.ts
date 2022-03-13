da224ad3616c614d9d7636a4f3b016568c555b8c
Arman Sarvadin <78083072+armansarv07@users.noreply.github.com>
2022-03-13 21:54:35 +0600

    Lab 4

---
+++ lab4/src/app/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
